5889097954b97dea61736234b22bfa7471d0a18f
Kajal Kumari <kajalkumari979834@gmail.com>
2026-08-29 22:28:43 +0530

    Add files via upload

---
+++ backend /src/router.py
@@ -0,0 +1,9 @@
+﻿from fastapi import APIRouter
+from src.api.routes import chat, auth, convert, history, files
+
+api_router = APIRouter(prefix="/api")
+api_router.include_router(chat.router)
+api_router.include_router(auth.router)
+api_router.include_router(convert.router)
+api_router.include_router(history.router)
+api_router.include_router(files.router)
